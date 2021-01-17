6819f75971a11a10eb6279acffea5b0c6ac06906
Amir Pahadi <apahadi73@gmail.com>
2021-01-17 04:34:17 -0600

    for some reason, ts was asking for @types/mongoose even though it was already there

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,47 +1,5 @@
-import express from "express";
-import cookieSession from "cookie-session";
-import colors from "colors";
-// notifies express for async errors
-// without this module, we had to use next function which could confuse peers
-import "express-async-errors";
-
 import mongoose from "mongoose";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middlewares/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-app.set("trust proxy", true); // trust first proxy
-// reads request json body
-app.use(express.json());
-
-// uses cookie session
-app.use(
-  cookieSession({
-    // since we are going to use jwt, we disabled the configuration on this cookie
-    signed: false,
-    // cookies can only be used through https connection
-    secure: true,
-  })
-);
-
-// routes
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// we don't have to use next here because of the express-async-error module
-app.all("*", async (req, res) => {
-  throw new NotFoundError();
-});
-
-// middleware - error handlers
-app.use(errorHandler);
+import { app } from "./app";
 
 // start the mongoose connection here
 const start = async () => {
@@ -62,7 +20,7 @@ const start = async () => {
   }
   // listner
   app.listen(3000, () => {
-    console.log(colors.green.bold("Listening on port 3000!"));
+    console.log("Listening on port 3000!");
   });
 };
 
