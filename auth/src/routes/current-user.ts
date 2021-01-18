dbea1f169f0f3a3eae02b5009cc63037d6478dca
Amir Pahadi <apahadi73@gmail.com>
2021-01-17 23:32:29 -0600

    load-balancer issue solved. forgot to remote the requireauth from current user route

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -5,14 +5,9 @@ import { requireAuth } from "../middlewares/require-auth";
 
 const router = express.Router();
 
-router.get(
-  "/api/users/currentUser",
-  currentUserManager,
-  requireAuth,
-  (req, res) => {
-    res.send({ currentUser: req.currentUser || null });
-  }
-);
+router.get("/api/users/currentUser", currentUserManager, (req, res) => {
+  res.send({ currentUser: req.currentUser || null });
+});
 
 // we are renaming the router to avoid name collisions
 export { router as currentUserRouter };
