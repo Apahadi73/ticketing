a86ac041a0ead213792a4bc831b12c628ad877b1
Amir Pahadi <apahadi73@gmail.com>
2021-01-22 23:16:07 -0600

    Updates

--- common/src/events/events/ticket-created-publisher.ts
+++
@@ -1,9 +0,0 @@
-import { TicketCreatedEvent } from "../tickets-srv-events/ticket-created-event";
-import { Subjects } from "../types/subjects";
-import { Publisher } from "../bases/publisher";
-
-// creates new TicketCreatedPublisher object and listens to the subscription channel of "ticket:created"
-export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-  // enforces subject to be of following type
-  subject: Subjects.TicketCreated = Subjects.TicketCreated;
-}
