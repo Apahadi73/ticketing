feaa0e377d7b662e6eb403714520c04a883573a3
Amir Pahadi <apahadi73@gmail.com>
2021-01-23 21:37:21 -0600

    working on different services custom listner

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -10,10 +10,11 @@ import {
 import { body } from "express-validator";
 import { Ticket } from "../models/ticket";
 import { Order } from "../models/order";
+import { OrderCreatedPublisher } from "../events/publishers/order-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
-// order expiration timer is set to 15 minutes
 const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
@@ -56,6 +57,17 @@ router.post(
     await order.save();
 
     // Publish an event saying that an order was created
+    new OrderCreatedPublisher(natsWrapper.client).publish({
+      id: order.id,
+      status: order.status,
+      userId: order.userId,
+      // uses utc timestamp
+      expiresAt: order.expiresAt.toISOString(),
+      ticket: {
+        id: ticket.id,
+        price: ticket.price,
+      },
+    });
 
     res.status(201).send(order);
   }
